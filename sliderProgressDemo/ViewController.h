8efc96a5a04ecb333b89834b39d431133cc3ecd2
liuwentian <liuwentian@wzdai.com>
2018-01-29 13:54:07 +0800

    Initial Commit

---
+++ sliderProgressDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sliderProgressDemo
+//
+//  Created by Raymon on 2018/1/29.
+//  Copyright © 2018年 Raymon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sliderProgressDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  sliderProgressDemo
+//
+//  Created by Raymon on 2018/1/29.
+//  Copyright © 2018年 Raymon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
